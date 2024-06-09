f78a286b3a48f49b36af2ec0fff6bb33ac7afcfd
Gigi <donghuege@hotmail.com>
2024-06-09 05:02:18 -0500

    updated notebook and app

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # Sprint_4_Project
+
+This project is allows the user to look at data collected about the stats and performances of a large collection of games hosted on the Steam platform
+The main analysis of the data compares genres, game titles, and developers against reviews, user percentage ratings, and awards given. 
+
+USER INSTRUCTIONS
+ OPTION 1
+Download the files from github (https://github.com/forest-goblin/Sprint_4_Project)
+Open the app.py file on VS Code
+Run and open with URL or Local URL
+
+ OPTION 2
+
+ Follow link to render (https://sprint-4-project-n1jh.onrender.com)
--- app.py
+++ app.py
@@ -1,65 +1,84 @@
 import streamlit as st
 
-
-
 import plotly.express as px
 import streamlit as st
 import pandas as pd
 import scipy.stats
 
-
 import dash
 from dash import Dash
-import dash_core_components as dcc
+from dash import dcc
 from dash import html
 from dash.dependencies import Input, Output, State
 
+
 steam = pd.read_csv('C:/Users/Donah/Documents/git/projects/Sprint_4_Project/steam_games.csv')
 
-app =Dash(__name__)
+st.header('Steam Store Data', divider='violet')
 
-app.layout = html.Div([
-    html.Div([
-        html.Label(['Game Genres']),
-        dcc.Dropdown(
-            id='my_drop',
-            options=[
-                {'label':'Recent Reviews', 'value' : 'recent_review'},
-                {'label':'Overall Reviews', 'value' : 'overall_review'},
-                {'label':'Awards', 'value' : 'awards'},
-                {'label':'Overall Review Percent', 'value' : 'overall_review_%'},
-            ],
-            value='overall_review',
-            multi=False,
-            clearable=False,
-            style={'width':'50%'}
-        ),
-     ]),
+#Header info
 
 
-    html.Div([
-         dcc.Graph(id='the_graph')
-    ]),
+best_rec = steam[['overall_review_count', 'title']]
+best_rec = best_rec.dropna()
+best_rec= best_rec.sort_values(by='overall_review_count',ascending=False).head(20)
+#games with the most reviews
 
-])
+
+highest_rev = steam[['overall_review_%', 'title']]
+highest_rev = highest_rev.dropna()
+highest_rev = highest_rev.sort_values(by='overall_review_%',ascending=False).head(20)
+#games with the highest review scores
 
 
-@app.callback(
-    Output(component_id='the_graph', component_property='figure'),
-    [Input(component_id='my_drop', component_property='value')]
-)
+c1 = pd.DataFrame(best_rec, columns=["number_of_reviews", 'title'])
+c2 =pd.DataFrame(highest_rev, columns=['percent', 'title2'])
+c1 = best_rec.loc[~best_rec.index.duplicated(keep='first')]
+c2 = highest_rev.loc[~highest_rev.index.duplicated(keep='first')]
 
-def update_graph(my_drop):
-    steam_info = steam
+combine_rec_rev = pd.concat([c1, c2], axis=0)
+#merging data for graph 
 
-    piechart=px.pie(
-        data_frame=steam_info,
-        names=my_drop,
-        hole=.3,
-        )
-    
-    return(piechart)
+count_g = pd.Series(steam['genres']).value_counts() 
+most_gen = count_g.head(20)
+bar_comp =[most_gen,[steam[['overall_review_count', 'overall_review_%']]]]
+#Seperating data for graph
 
-if __name__ == '__main__':
-    app.run_server(debug=True)
-    
+count_d =  steam[['developer', 'title']]
+most_games = count_d.dropna()
+dev_comp = most_games.sort_values(by='developer',ascending=False).head(20)
+
+b1 = pd.DataFrame(dev_comp, columns=['developer', 'title'])
+b1 = dev_comp.loc[~dev_comp.index.duplicated(keep='first')] 
+d1=pd.DataFrame(highest_rev, columns=['percent', 'title2'])
+d1=highest_rev.loc[~highest_rev.index.duplicated(keep='first')]
+
+combine_dev = pd.concat([b1,d1], axis=0)
+#merging data for graph
+
+
+st.write("Through these graphs you will be able to explore the performance of game titles and developers on the gaming platform Steam.")
+st.divider() 
+#Graphing 
+fig = px.bar(combine_rec_rev, x="title", y="overall_review_count", color="overall_review_%", barmode="group")
+st.write(fig)
+st.divider() 
+
+fig = px.scatter(steam, x="title", y="awards", color="overall_review_%")
+st.write(fig)
+
+
+switch = st.checkbox("Look at Developer data")
+switch2 = st.checkbox("Look at Game Title data")
+
+fig1 = px.bar(combine_dev, x="developer", y="title", color="overall_review_%", barmode="group")
+fig2 = px.bar(combine_rec_rev, x="title", y="overall_review_count", color="overall_review_%", barmode="group")
+
+
+if switch:
+    st.write(fig1)
+
+if switch2:
+    st.write(fig2)
+
+#Creating Check Boxes
