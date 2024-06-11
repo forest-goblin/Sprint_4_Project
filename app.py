0eca16bbbb1bf12af14f4621de3c4131306983ed
Gigi <donghuege@hotmail.com>
2024-06-11 01:40:20 -0500

    updates

--- app.py
+++ app.py
@@ -19,11 +19,16 @@ st.divider()
 #Header info
 
 
+
 best_rec = steam[['overall_review_count', 'title']]
 best_rec = best_rec.dropna()
 best_rec= best_rec.sort_values(by='overall_review_count',ascending=False).head(20)
 #games with the most reviews
 
+top_awards = steam[['awards','genres']]
+top_awards = top_awards.dropna()
+top_awards = top_awards.sort_values('awards',ascending=False).head(20)
+#top awards
 
 highest_rev = steam[['overall_review_%', 'title']]
 highest_rev = highest_rev.dropna()
@@ -60,18 +65,68 @@ combine_dev = pd.concat([b1,d1], axis=0)
 st.write("Through these graphs you will be able to explore the performance of game titles and developers on the gaming platform Steam.")
 st.divider() 
 #Graphing 
-fig = px.bar(combine_rec_rev, x="title", y="overall_review_count", color="overall_review_%", barmode="group")
+fig = px.bar(combine_rec_rev, x="title", y="overall_review_count",title="Top 20 Games with their Review Count and Review Percentage ", barmode="group",labels={
+                     "title": "Game Titles",
+                     "overall_review_count":"Review Count",
+                 },
+             )
 st.write(fig)
 st.divider() 
 
-fig = px.scatter(steam, x="title", y="awards", color="overall_review_%")
+fig = px.scatter(top_awards, x="genres", y="awards",title="Top 20 genres with the most awards")
 st.write(fig)
 
 
 switch = st.checkbox("Look at Developer data")
 switch2 = st.checkbox("Look at Game Title data")
 
-fig1 = px.bar(combine_dev, x="developer", y="title", color="overall_review_%", barmode="group")
+#fig1 = px.bar(combine_dev, x="developer", y="title", color="overall_review_%", barmode="group")
+app =Dash(__name__)
+
+fig1 = app.layout = html.Div([
+    html.Div([
+        html.Label(['Game Genres']),
+        dcc.Dropdown(
+            id='my_drop',
+            options=[
+                {'label':'Recent Reviews', 'value' : 'recent_review'},
+                {'label':'Overall Reviews', 'value' : 'overall_review'},
+                {'label':'Awards', 'value' : 'awards'},
+                {'label':'Overall Review Percent', 'value' : 'overall_review_%'},
+            ],
+            value='overall_review',
+            multi=False,
+            clearable=False,
+            style={'width':'50%'}
+        ),
+     ]),
+
+
+    html.Div([
+         dcc.Graph(id='the_graph')
+    ]),
+
+])
+
+
+@app.callback(
+    Output(component_id='the_graph', component_property='figure'),
+    [Input(component_id='my_drop', component_property='value')]
+)
+
+def update_graph(my_drop):
+    steam_info = steam
+
+    piechart=px.pie(
+        data_frame=steam_info,
+        names=my_drop,
+        hole=.3,
+        )
+    
+    return(piechart)
+
+
+
 fig2 = px.bar(combine_rec_rev, x="title", y="overall_review_count", color="overall_review_%", barmode="group")
 
 
